7d436475ed9fee62d31907cd111a251d62030745
Adrian <adrian.kiermaier@gmail.com>
2022-03-01 16:50:59 +0100

    u: barebones Webserver is running

--- webserver/main/views.py
+++ webserver/main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here
+
+def index(response):
+    return HttpResponse("<h1>Hello your computer has virus, please send 100 bitcoins to Adrian immidiately</h1>")
