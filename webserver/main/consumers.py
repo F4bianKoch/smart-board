9d02a277da4429c1e2f876d83f7caa38f72fbd06
Fabian <fabian.cook@web.de>
2022-03-11 21:30:45 +0100

    u: used threading to disconnect websocket

--- webserver/main/consumers.py
+++ webserver/main/consumers.py
@@ -1,6 +1,8 @@
+import imp
 import json
 from datetime import datetime
 import pytz
+import threading
 
 from channels.generic.websocket import WebsocketConsumer
 
@@ -8,17 +10,23 @@ from channels.generic.websocket import WebsocketConsumer
 class TimeConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
-        old_time = 0
+        self.stop = False
 
-        try:
-            while True:
-                time_query = datetime.now(pytz.timezone('Europe/Berlin'))
-                current_time = time_query.strftime('%H:%M')
-                if current_time != old_time:
-                    self.send(json.dumps({'time': str(current_time)}))
-                old_time = current_time
-        except Exception as e:
-            print(e)
+        self.thread = threading.Thread(target=self.get_time)
+        self.thread.start()
+
+    def get_time(self):
+        old_time = 0
+        while True:
+            time_query = datetime.now(pytz.timezone('Europe/Berlin'))
+            current_time = time_query.strftime('%H:%M')
+            if current_time != old_time:
+                self.send(json.dumps({'time': str(current_time)}))
+            old_time = current_time
+            if self.stop:
+                break
 
     def disconnect(self, close_code):
+        self.stop = True
+        del self.thread
         print('websocket disconntected')
