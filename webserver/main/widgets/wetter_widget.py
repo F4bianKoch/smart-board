103e1837c240a5c21df8e5418c0d3e992b34946c
Fabian <fabian.cook@web.de>
2022-03-23 19:18:04 +0100

    a: initial design for the weather_widget

--- webserver/main/widgets/wetter_widget.py
+++ webserver/main/widgets/wetter_widget.py
@@ -8,23 +8,32 @@ class WeatherWidget:
 
     def api_request(self):
         weather_json = get(
-            f'''https://api.weatherapi.com/v1/current.json?key={self.__api_key}&lang=de&q={self.location}''')
+            f'''https://api.weatherapi.com/v1/forecast.json?key={self.__api_key}&lang=de&q={self.location}&days=2''')
         weather_json = weather_json.json()
         return weather_json
 
     def format_request_small(self, weather_json):
+        print(weather_json)
         current_temperature = weather_json['current']['temp_c']
         weather_icon_link = weather_json['current']['condition']['icon']
         text = weather_json['current']['condition']['text']
-        humidity = weather_json['current']['humidity']
+        humidity = weather_json['forecast']['forecastday'][0]['day']['daily_chance_of_rain']
         location = weather_json['location']['name'] + \
             ', ' + weather_json['location']['region']
+        tomorrow_max_temperature = weather_json['forecast']['forecastday'][1]['day']['maxtemp_c']
+        tomorrow_min_temperature = weather_json['forecast']['forecastday'][1]['day']['mintemp_c']
+        tomorrow_text = weather_json['forecast']['forecastday'][1]['day']['condition']['text']
+        tomorrow_icon = weather_json['forecast']['forecastday'][1]['day']['condition']['icon']
 
-        current_weather = {
+        weather = {
             'temperature': current_temperature,
             'icon': weather_icon_link,
             'text': text,
             'humidity': humidity,
-            'location': location
+            'location': location,
+            'tomorrow_max_temperature': tomorrow_max_temperature,
+            'tomorrow_min_temperature': tomorrow_min_temperature,
+            'tomorrow_icon': tomorrow_icon,
+            'tomorrow_text': tomorrow_text
         }
-        return current_weather
+        return weather
