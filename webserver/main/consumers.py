da713e53d4d681085993e32f161263a572f56b91
Fabian <fabian.cook@web.de>
2022-03-26 20:33:17 +0100

    changed consumer architecture II.

--- webserver/main/consumers.py
+++ webserver/main/consumers.py
@@ -10,8 +10,11 @@ from .widgets.time_feature import current_time
 from .widgets.wetter_widget import WeatherWidget
 
 
-class TimeConsumer(WebsocketConsumer):
+class ScreensaverConsumer(WebsocketConsumer):
+    ''' Websocket for the screensaver '''
+
     def connect(self):
+        ''' on websocket connect '''
         self.accept()
         self.stop = False
 
@@ -19,79 +22,95 @@ class TimeConsumer(WebsocketConsumer):
         self.thread_time_display.start()
 
     def get_time(self):
+        ''' get current time and send it to websocket '''
         old_time = 0
+
         while True:
+            # querry time
             time_query = datetime.now(pytz.timezone('Europe/Berlin'))
             current_time = time_query.strftime('%H:%M')
+            # format querry output
             day = time_query.strftime('%a')
             current_date = time_query.strftime(f'{day[0:2]} %d.%m.%Y')
+            # send to websocket
             if current_time != old_time:
                 self.send(json.dumps({
                     'time': str(current_time),
                     'date': str(current_date),
                 }))
             old_time = current_time
+            # break if websocket was disconnected
             if self.stop:
                 break
 
     def disconnect(self, code):
+        ''' on websocket disconnect '''
         self.stop = True
         del self.thread_time_display
-        print('websocket disconntected')
 
 
-class WeatherWidgetConsumer(WebsocketConsumer):
+class HomePageConsumer(WebsocketConsumer):
+    ''' Websocket for the homepage '''
+
     def connect(self):
+        ''' on websocket connect '''
         self.accept()
+        self.stop = False
+        content = {}
+
+        # setup for weather widget
         f = open('main/config.json')
         config = json.load(f)
+        f.close()
         api_key = config["weather_widget"]["api_key"]
         location = config["weather_widget"]["location"]
-        self.stop = False
-        weather_widget = WeatherWidget(
-            api_key=api_key, location=location)
-        self.thread_weather_widget = threading.Thread(
-            target=self.get_weather, args=[weather_widget])
-        self.thread_weather_widget.start()
+        weather_widget = WeatherWidget(api_key, location)
 
-    def get_weather(self, weather_widget):
+        # start all threads for the widgets
+        self.thread_time = threading.Thread(
+            target=self.get_time, args=[content])
+        self.thread_weather_widget = threading.Thread(
+            target=self.get_weather, args=[weather_widget, content])
+        self.thread_weather_widget.start()
+        self.thread_time.start()
+
+    def get_weather(self, weather_widget, content):
+        ''' get current weather every 20min '''
         while True:
             weather_json = weather_widget.api_request()
             weather = weather_widget.format_request_small(weather_json)
-            self.send(json.dumps(weather))
+            content.update(weather)
+            self.send(json.dumps(content))
             if self.stop:
                 break
             time.sleep(1200)
 
-    def disconnect(self, code):
-        self.stop = True
-        del self.thread_weather_widget
-        print('websocket disconntected')
-
-
-class TimeWidgetConsumer(WebsocketConsumer):
-
-    def connect(self):
-        print('connected')
-        self.accept()
-        self.stop = False
-
-        self.thread_time_widget = threading.Thread(target=self.get_time)
-        self.thread_time_widget.start()
-
-    def get_time(self):
+    def get_time(self, content):
+        ''' get current time and send it to websocket '''
         old_time = 0
+
         while True:
-            cur_time = current_time()
-            if cur_time != old_time:
-                self.send(json.dumps({
-                    'time': str(cur_time),
-                }))
-            old_time = cur_time
+            # querry time
+            time_query = datetime.now(pytz.timezone('Europe/Berlin'))
+            current_time = time_query.strftime('%H:%M')
+            # format querry output
+            day = time_query.strftime('%a')
+            current_date = time_query.strftime(f'{day[0:2]} %d.%m.%Y')
+            # send to websocket
+            if current_time != old_time:
+                time = {
+                    'time': str(current_time),
+                    'date': str(current_date),
+                }
+                content.update(time)
+                self.send(json.dumps(content))
+            old_time = current_time
+            # break if websocket was disconnected
             if self.stop:
                 break
 
     def disconnect(self, code):
+        ''' on websocket disconnect '''
         self.stop = True
-        del self.thread_time_widget
-        print('websocket disconntected')
+        del self.thread_time
+        del self.thread_weather_widget
