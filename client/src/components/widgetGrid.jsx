0e24702a0479ca4d4697569c5a0c579f47172113
Adrian <adrian.kiermaier@gmail.com>
2022-07-20 13:43:33 +0200

    a: todoWidget to WidgetGrid

--- client/src/components/widgetGrid.jsx
+++ client/src/components/widgetGrid.jsx
@@ -1,6 +1,7 @@
 import './styles/widgetGrid.css';
 import ExampleWidget from './exampleWidget';
 import WeatherWidget from './weatherWidget';
+import TodoWidget from './todoWidget';
 
 
 const WidgetGrid = () => {
@@ -8,7 +9,7 @@ const WidgetGrid = () => {
   return (
     <div className="widget-grid">
       <div className="widget"><WeatherWidget /></div>
-      <div className="widget"><ExampleWidget /></div>
+      <div className="widget"><TodoWidget/></div>
       <div className="widget"><ExampleWidget /></div>
       <div className="widget"><ExampleWidget /></div>
     </div>    
